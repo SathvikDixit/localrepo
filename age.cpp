fc36a92f52f45673e0f73aae50bd1f0f9cc52250
SathvikDixit <sathvikdixit21@gmail.com>
2024-01-13 11:26:58 +0530

    Age cpp prog

---
+++ age.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c,x;
+    cout << "Enter any code:";
+    cin >> c;
+    if(c>=18)
+    {
+        cout <<" YOu are eligable";
+    }
+    else{
+        cout << "you are  not eligable";
+    }
+    return 0;
+}
